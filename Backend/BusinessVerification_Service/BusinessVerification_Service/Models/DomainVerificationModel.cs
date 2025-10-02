45f59a68be6ecf1a173450e91ce81611b5a0fa94
Christiaan Huisman <huismanchristiaanwork@gmail.com>
2025-10-02 16:42:27 +0200

    Initial model for domain verification method

---
+++ Backend/BusinessVerification_Service/BusinessVerification_Service/Models/DomainVerificationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessVerification_Service.Models
+{
+    public class DomainVerificationModel
+    {
+        public string UserId { get; set; }
+        public string BusinessEmail { get; set; }
+        public string BusinessWebsite { get; set; }
+        public string BusinessName { get; set; }
+        public string VerificationMethod { get; set; } // e.g. domain
+    }
+}
